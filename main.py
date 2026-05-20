f4e51c3ff211b98f9eca9e12998ca7f8d6e30edd
Julio7-7-7 <julio.toledo2030@gmail.com>
2026-05-20 16:38:44 -0400

    refactor: añadiendo campo foto a programas y conversión de a base64 para su almacenamiento y futura refactorización para el servidor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from routers import all_routers
 from fastapi.middleware.cors import CORSMiddleware
+from pathlib import Path
 
 app = FastAPI()
 
@@ -14,6 +16,10 @@ app.add_middleware(
 
 for router in all_routers:
     app.include_router(router)
+
+MEDIA_ROOT = Path(__file__).parent / "media"
+MEDIA_ROOT.mkdir(exist_ok=True)
+app.mount("/media", StaticFiles(directory=str(MEDIA_ROOT)), name="media")
 
 @app.get("/")
 def root():
